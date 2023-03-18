4c5a74fdb0b615dc8e5d9e778ffa2bfe3c1854fa
iconoyuri <iconoyuri@gmail.com>
2023-03-18 11:00:03 +0100

    icon demo

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+	return (
+		<div>
+			<FontAwesomeIcon icon={faEnvelope} />
+		</div>
+	)
+}
 
-export default App;
+export default App
